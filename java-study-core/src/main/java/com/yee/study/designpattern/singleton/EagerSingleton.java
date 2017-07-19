383bfa6c6b7bd8226a0544744e16be0344a9b6e6
rogeryee <roger.yee@hotmail.com>
2017-07-19 17:43:12 +0800

    新增设计模式/单例模式/创建者模式

---
+++ java-study-core/src/main/java/com/yee/study/designpattern/singleton/EagerSingleton.java
@@ -0,0 +1,20 @@
+package com.yee.study.designpattern.singleton;
+
+/**
+ * 饥饿型：存在潜在的性能问题
+ *
+ * @author RogerYee
+ */
+public class EagerSingleton
+{
+    private static EagerSingleton singObj = new EagerSingleton();
+
+    private EagerSingleton()
+    {
+    }
+
+    public static EagerSingleton getSingleInstance()
+    {
+        return singObj;
+    }
+}
