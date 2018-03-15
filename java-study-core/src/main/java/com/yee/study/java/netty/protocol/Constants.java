aae6c6a344811c38a33c37dcd74a35043d25fdca
rogeryee <roger.yee@hotmail.com>
2018-03-15 17:55:16 +0800

    增加文件服务、自定义协议栈的例子

--- java-study-core/src/main/java/com/yee/study/java/netty/protocol/Constants.java
+++ java-study-core/src/main/java/com/yee/study/java/netty/protocol/Constants.java
@@ -7,9 +7,9 @@ public class Constants
 {
     public static final String REMOTEIP = "127.0.0.1";
 
-    public static final int PORT = 8080;
+    public static final int PORT = 8089;
 
-    public static final int LOCAL_PORT = 12088;
+    public static final int LOCAL_PORT = 8088;
 
     public static final String LOCALIP = "127.0.0.1";
 }
