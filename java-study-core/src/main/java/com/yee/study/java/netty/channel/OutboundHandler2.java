56fd153b5e89641299b214586afbe4b3fe1120bd
rogeryee <roger.yee@hotmail.com>
2018-03-22 13:59:59 +0800

    增加Netty Channel的例子

---
+++ java-study-core/src/main/java/com/yee/study/java/netty/channel/OutboundHandler2.java
@@ -0,0 +1,23 @@
+package com.yee.study.java.netty.channel;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Roger.Yi
+ */
+public class OutboundHandler2 extends ChannelOutboundHandlerAdapter
+{
+    private static Logger logger = LoggerFactory.getLogger(OutboundHandler2.class);
+
+    @Override
+    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception
+    {
+        logger.info("OutboundHandler2.write");
+        // 执行下一个OutboundHandler
+        super.write(ctx, msg, promise);
+    }
+}
