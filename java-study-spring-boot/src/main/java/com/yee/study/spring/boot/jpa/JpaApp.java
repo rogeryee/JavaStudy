5cd8201d9459c90044e7a856b395d65c0680acb0
rogeryee <roger.yee@hotmail.com>
2018-06-18 16:35:06 +0800

    增加SpringBoot JPA

---
+++ java-study-spring-boot/src/main/java/com/yee/study/spring/boot/jpa/JpaApp.java
@@ -0,0 +1,15 @@
+package com.yee.study.spring.boot.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * JPA示例入口程序
+ * @author Roger.Yi
+ */
+@SpringBootApplication
+public class JpaApp {
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApp.class, args);
+    }
+}
