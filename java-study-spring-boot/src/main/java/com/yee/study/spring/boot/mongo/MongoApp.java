0a599c66685224ac918e763d4c08d638d34018ce
rogeryee <roger.yee@hotmail.com>
2018-06-20 18:26:26 +0800

    增加SpringBoot Mongodb

---
+++ java-study-spring-boot/src/main/java/com/yee/study/spring/boot/mongo/MongoApp.java
@@ -0,0 +1,16 @@
+package com.yee.study.spring.boot.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Mongodb示例入口程序
+ *
+ * @author Roger.Yi
+ */
+@SpringBootApplication
+public class MongoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MongoApp.class, args);
+    }
+}
---
+++ java-study-spring-boot/src/main/java/com/yee/study/spring/boot/mongo/dao/UserInfoDao.java
@@ -0,0 +1,29 @@
+package com.yee.study.spring.boot.mongo.dao;
+
+import com.yee.study.spring.boot.mongo.model.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Roger.Yi
+ */
+@Repository
+public class UserInfoDao {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public void save(UserInfo userInfo) {
+        mongoTemplate.save(userInfo);
+    }
+
+    public UserInfo findByName(String name) {
+        Query query = new Query(Criteria.where("name").is(name));
+        UserInfo userInfo = mongoTemplate.findOne(query, UserInfo.class);
+        return userInfo;
+    }
+}
