4ebe90245e2119a4f6ebb47c760dabbb7147c987
rogeryee <roger.yee@hotmail.com>
2018-06-21 14:13:44 +0800

    增加SpringBoot Redis

---
+++ java-study-spring-boot/src/main/java/com/yee/study/spring/boot/redis/RedisApp.java
@@ -0,0 +1,16 @@
+package com.yee.study.spring.boot.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Redis示例入口程序
+ *
+ * @author Roger.Yi
+ */
+@SpringBootApplication
+public class RedisApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApp.class, args);
+    }
+}
