28350f5ad960723e9a695910848abc34bce21846
rogeryee <roger.yee@hotmail.com>
2018-06-24 17:16:28 +0800

    增加Zookeeper Demo

--- java-study-zookeeper/src/main/java/com/yee/study/zookeeper/zk/ZkDemo.java
+++ java-study-zookeeper/src/main/java/com/yee/study/zookeeper/zk/ZkDemo.java
@@ -5,7 +5,12 @@ import org.apache.zookeeper.data.Stat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
+
+import static org.apache.zookeeper.Watcher.Event.EventType;
+import static org.apache.zookeeper.Watcher.Event.KeeperState;
+import static org.apache.zookeeper.ZooDefs.Ids;
 
 /**
  * 使用原生Zookeeper包示例
@@ -26,30 +31,40 @@ public class ZkDemo implements Watcher {
         zk = new ZooKeeper("localhost:2181", 5000, new ZkDemo());
         cdl.await();
 
-//        doCreateData();
-//        doDataChange();
+        doCreateData("/zk-test-");
+//        doDataChange("/zk-test");
+//        doChildNodeData("/zk-test");
+
 
         Thread.sleep(Integer.MAX_VALUE);
     }
 
     /**
      * 创建节点
+     *
      * @throws Exception
      */
-    private static void doCreateData() throws Exception {
-        String path1 = zk.create("/zk-test-", "123".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+    private static void doCreateData(String path) throws Exception {
+        String path1 = zk.create(path, "123".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
         logger.info("create path=[{}] end.", path1);
 
-        String path2 = zk.create("/zk-test-", "456".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        String path2 = zk.create(path, "456".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
         logger.info("create path=[{}] end.", path2);
+
+        zk.create(path, "789".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL, new AsyncCallback.StringCallback() {
+            @Override
+            public void processResult(int rc, String path, Object ctx, String name) {
+                logger.info("create path result : " + rc + ", path = " + path + ", ctx = " + ctx + ", name = " + name);
+            }
+        }, "I am context");
     }
 
     /**
      * 修改数据
+     *
      * @throws Exception
      */
-    private static void doDataChange() throws Exception {
-        String path = "/zk-test";
+    private static void doDataChange(String path) throws Exception {
         zk.getData(path, true, stat);
         logger.info("get path=[{}] : czxid=[{}], mzxid=[{}], version=[{}]", path, stat.getCzxid(), stat.getMzxid(), stat
                 .getVersion());
@@ -57,17 +72,42 @@ public class ZkDemo implements Watcher {
         zk.setData("/zk-test", "123".getBytes(), -1);
     }
 
+    /**
+     * 操作子节点
+     *
+     * @throws Exception
+     */
+    private static void doChildNodeData(String path) throws Exception {
+        zk.create(path, "123".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+
+        int size = 3;
+        for (int i = 1; i <= size; i++) {
+            String childPath = path + "/" + "c" + i;
+            logger.info("create child path=[{}]", childPath);
+            zk.create(childPath, String.valueOf(i).getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+        }
+
+        List<String> list = zk.getChildren(path, true);
+        for (String str : list) {
+            logger.info("get child value=[{}]", str);
+        }
+    }
+
     @Override
     public void process(WatchedEvent event) {
-        try {
-            logger.info("Receive watched event: state=[{}] type=[{}]", event.getState(), event.getType());
+        logger.info("Receive watched event: state=[{}] type=[{}]", event.getState(), event.getType());
 
-            if (Event.KeeperState.SyncConnected == event.getState()) {
-                if (Event.EventType.None == event.getType() && null == event.getPath()) {
+        try {
+            if (event.getState() == KeeperState.SyncConnected) {
+                if (event.getType() == EventType.None && null == event.getPath()) {
                     cdl.countDown();
-                } else if (event.getType() == Event.EventType.NodeDataChanged) {
+                } else if (event.getType() == EventType.NodeDataChanged) {
                     zk.getData(event.getPath(), true, stat);
                     logger.info("set path=[{}] end : czxid=[{}], mzxid=[{}], version=[{}]", event.getPath(), stat.getCzxid(), stat
+                            .getMzxid(), stat.getVersion());
+                } else if (event.getType() == EventType.NodeChildrenChanged) {
+                    zk.getChildren(event.getPath(), true, stat);
+                    logger.info("get child path=[{}] end : czxid=[{}], mzxid=[{}], version=[{}]", event.getPath(), stat.getCzxid(), stat
                             .getMzxid(), stat.getVersion());
                 }
             }
