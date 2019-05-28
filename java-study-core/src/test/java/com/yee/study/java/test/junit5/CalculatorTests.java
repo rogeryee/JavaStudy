eadef7b64227e102677c1b2a9b19d11ddbcd5f89
rogeryee <roger.yee@hotmail.com>
2019-05-28 10:23:55 +0800

    upgrade to Junit5

--- java-study-core/src/test/java/com/yee/study/java/test/junit5/CalculatorTests.java
+++ java-study-core/src/test/java/com/yee/study/java/test/junit5/CalculatorTests.java
@@ -3,6 +3,8 @@ package com.yee.study.java.test.junit5;
 import com.yee.study.java.test.mock.Calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,5 +18,18 @@ public class CalculatorTests {
     void addsTwoNumbers() {
         Calculator calculator = new Calculator();
         assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+    }
+
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "0,    1,   1",
+            "1,    2,   3",
+            "49,  51, 100",
+            "1,  100, 101"
+    })
+    void add(int first, int second, int expectedResult) {
+        Calculator calculator = new Calculator();
+        assertEquals(expectedResult, calculator.add(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
     }
 }
