6badc7b46022a8d38571fbe4c722bd3e14f84602
yish <yish@tpl.cntaiping.com>
2020-11-19 11:28:22 +0800

    refactor dependencies

---
+++ java-study-spring-core/src/main/java/com/yee/study/spring/framework/utils/aop/EmployeeServiceImpl.java
@@ -0,0 +1,12 @@
+package com.yee.study.spring.framework.utils.aop;
+
+/**
+ * @author Roger.Yi
+ */
+public class EmployeeServiceImpl implements IEmployeeService {
+
+    @Override
+    public void doBiz() {
+
+    }
+}
