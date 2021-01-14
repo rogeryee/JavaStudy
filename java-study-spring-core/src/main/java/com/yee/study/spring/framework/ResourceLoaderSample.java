af94b58e68802ee1bab83f7685b64bde382d93bb
yish <yish@tpl.cntaiping.com>
2021-01-14 18:26:38 +0800

    增加了BeanFactory的类图

---
+++ java-study-spring-core/src/main/java/com/yee/study/spring/framework/ResourceLoaderSample.java
@@ -0,0 +1,32 @@
+package com.yee.study.spring.framework;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.lang.reflect.WildcardType;
+import java.util.List;
+
+/**
+ * ResourceLoader 示例
+ *
+ * @author Roger.Yi
+ */
+public class ResourceLoaderSample {
+
+    private static final Logger log = LoggerFactory.getLogger(ResolvableTypeTest.class);
+
+    /**
+     *
+     */
+    @Test
+    public void testResourceLoader() throws Exception {
+    }
+}
